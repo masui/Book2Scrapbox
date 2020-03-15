0c8ac7a470430ec8c618a37c7622ee4ef7149675
Toshiyuki Masui <masui@pitecan.com>
2020-03-16 07:22:25 +0900

    addocr.rbわかりやすく

--- addocr.rb
+++ addocr.rb
@@ -1,55 +1,59 @@
 #
-# 自炊本のScrapboxにOCRデータを追加
+# ScrapboxページにGyazoが含まれているときOCRテキストを追加
+#
+# 手順
+#  1. ScrapboxプロジェクトのJSONデータをエクスポートする
+#  2. GyazoのOCRテキストをデータに追加する
+#     % ruby addocr.rb origdata.json > newdata.json
+#  3. 作成されたJSONデータ(newdata.json)をインポートする
 #
 
 require 'json'
 require 'gyazo'
 
 jsonfile = ARGV[0]
-unless File.exist?(jsonfile)
+unless jsonfile && File.exist?(jsonfile)
   STDERR.puts "JSON file not found"
-  STDERR.puts "Usage: addocr orig.json > new.json"
+  STDERR.puts "Usage: ruby addocr.rb orig.json > new.json"
   exit
 end
 
-token = ENV['GYAZO_TOKEN'] # .bash_profileに書いてある
+token = ENV['GYAZO_TOKEN'] # Gyazo APIのトークン (.bash_profileに書いておく)
 gyazo = Gyazo::Client.new access_token: token
 
 origdata = JSON.parse(File.read(jsonfile))
 
-jsondata = {}
-pages = []
-jsondata['pages'] = pages
+newdata = {}
+newdata['pages'] = []
 
-origdata['pages'].each { |page|
-  STDERR.puts page['title']
+origdata['pages'].each { |origpage|
+  STDERR.puts origpage['title']
   newpage = {}
-  newpage['title'] = page['title']
-  lines = []
-  newpage['lines'] = lines
+  newpage['title'] = origpage['title']
+  newpage['lines'] = []
   gyazoid = nil
   ocr_done = false
-  page['lines'].each { |line|
-    lines << line
-    if line =~ /gyazo\.com\/([0-9a-f]{32})/i
+  origpage['lines'].each { |origline|
+    newpage['lines'] << origline
+    if origline =~ /gyazo\.com\/([0-9a-f]{32})/i
       gyazoid = $1
     end
-    if line =~ /OCR text/
+    if origline =~ /OCR text/
       ocr_done = true
     end
   }
   if gyazoid && !ocr_done
-    lines << ""
-    lines << "(OCR text)"
+    newpage['lines'] << ""
+    newpage['lines'] << "(OCR text)"
     res = gyazo.image image_id: gyazoid
     if res && res[:ocr]
-      res[:ocr][:description].split(/\n/).each { |line|
-        lines << "> #{line}"
+      res[:ocr][:description].split(/\n/).each { |ocrline|
+        newpage['lines'] << "> #{ocrline}"
       }
     end
   end
 
-  pages << newpage
+  newdata['pages'] << newpage
 }
 
-puts jsondata.to_json
+puts newdata.to_json
